854e0da41417c11a9671f8c4d0d888ec8d51950c
Allan Cheng <allanhfcheng@gmail.com>
2019-09-21 18:29:35 -0700

    done

---
+++ src/bookmarks/bookmark-service.js
@@ -0,0 +1,9 @@
+/*const BookmarksService = {
+    getAllBookmarks(knex) {
+      return knex.select('*').from('bookmarks')
+    },
+
+    getById(knex, id) {
+        return knex.from('bookmarks').select('*').where('id', id).first()
+      },
+}*/
